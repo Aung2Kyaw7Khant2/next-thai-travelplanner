9ec1d1342b53ce5383ed0e4e8ed3fb46cf70c723
Aung Kyaw Khant <95082621+Aung2Kyaw7Khant2@users.noreply.github.com>
2024-04-08 10:11:47 +0000

    test 2

--- Hello.py
+++ Hello.py
@@ -1,18 +1,5 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
+import datetime
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
@@ -25,26 +12,29 @@ def run():
     )
 
     st.write("# :balloon: Welcome to Streamlit! 👋")
+    start = st.sidebar.selectbox(
+    'Starting Point',
+    ('Email', 'Home phone', 'Mobile phone'))
+    destinations = st.sidebar.multiselect(
+    'Destination',
+    ['Green', 'Yellow', 'Red', 'Blue'])
+    today = datetime.datetime.now()
+    next_year = today.year + 1
+    jan_1 = datetime.date(next_year, 1, 1)
+    dec_31 = datetime.date(next_year, 12, 31)
 
-    st.sidebar.success("Select a demo above.")
-
-    # st.markdown(
-    #     """
-    #     Streamlit is an open-source app framework built specifically for
-    #     Machine Learning and Data Science projects.
-    #     **👈 Select a demo from the sidebar** to see some examples
-    #     of what Streamlit can do!
-    #     ### Want to learn more?
-    #     - Check out [streamlit.io](https://streamlit.io)
-    #     - Jump into our [documentation](https://docs.streamlit.io)
-    #     - Ask a question in our [community
-    #       forums](https://discuss.streamlit.io)
-    #     ### See more complex demos
-    #     - Use a neural net to [analyze the Udacity Self-driving Car Image
-    #       Dataset](https://github.com/streamlit/demo-self-driving)
-    #     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    # """
-    # )
+    duration = st.sidebar.date_input(
+        "Duration Date",
+        (jan_1, datetime.date(next_year, 1, 7)),
+        jan_1,
+        dec_31,
+        format="MM.DD.YYYY",
+    )
+    cost = st.sidebar.slider('Managing Your Trip Costs', 3000, 20000, 10000, 1000)
+    trip_type = st.sidebar.multiselect(
+    'Trip Type',
+    ['Green', 'Yellow', 'Red', 'Blue'])
+    additional_trip = st.text_input('Additional Trip Type')
 
 
 if __name__ == "__main__":
